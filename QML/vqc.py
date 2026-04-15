25a7c4c27fbabb0addd107b9d711cdbc7367b86c
Josh Celestino <114170790+JoshIsaiahAI@users.noreply.github.com>
2026-04-16 01:08:17 +1000

    Create vqc.py

---
+++ QML/vqc.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import pennylane as qml
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
